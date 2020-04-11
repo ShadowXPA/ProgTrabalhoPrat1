93f74c7b394db4f9a9776d39a230b8e9cd8fcd2b
ShadowXPA <pedro.aves@hotmail.com>
2020-04-11 18:27:15 +0100

    Adicionado mais locais para testar

--- ProgTrabPrat1/Place.c
+++ ProgTrabPrat1/Place.c
@@ -13,7 +13,7 @@
 
 ListPlace *init_places(const char *filename) {
     printf("\n Initializing List of places.\n");
-    ListPlace *places = calloc(1, sizeof(ListPlace));
+    ListPlace *places = calloc(1, sizeof (ListPlace));
     if (places == NULL) {
         printf("Error allocating memory for places!\n");
         return NULL;
@@ -31,18 +31,18 @@ ListPlace *init_places(const char *filename) {
     size_t read = 0;
     bool keep_reading = true;
     while (keep_reading) {
-        if ((read = fread(&place, sizeof(Place), 1, fp)) == 0) {
+        if ((read = fread(&place, sizeof (Place), 1, fp)) == 0) {
             // EOF
             keep_reading = false;
         } else {
-            if (!resize_places(places, (i + 1) * sizeof(Place))) {
+            if (!resize_places(places, (i + 1) * sizeof (Place))) {
                 printf("Error! Not able to allocate memory for places\n");
                 free_places(places);
                 fclose(fp);
                 return NULL;
             }
             places->place[i] = place;
-            places->size = ++i;//(i + 1);
+            places->size = ++i; //(i + 1);
             //i++;
         }
     }
@@ -109,24 +109,33 @@ bool _evaluate_list_connection(const ListPlace *places) {
      * In the end if all connections are OKAY, return true
      */
     for (uint32_t i = 0; i < places->size; i++) {
-        for (int j = 0; j < MAX_CONNECTIONS; j++) {
+        for (uint32_t j = 0; j < MAX_CONNECTIONS; j++) {
             if (places->place[i].connection[j] != -1) {
+                // Checkpoint = 1
+                // Place[i] has Connection[j]
+                uint8_t checkpoint = 1;
                 for (uint32_t k = 0; k < places->size; k++) {
-                    if (i != k) {
-                        if (places->place[i].connection[j] == places->place[k].id) {
-                            bool has_connection = false;
-                            for (int l = 0; l < MAX_CONNECTIONS; l++) {
-                                if (places->place[k].connection[l] == places->place[i].id) {
-                                    has_connection = true;
-                                    break;
-                                }
+                    if (i != k && places->place[i].connection[j] == places->place[k].id) {
+                        // Checkpoint = 2
+                        // Place[i] has confirmed Connection[j]
+                        checkpoint++;
+                        for (uint32_t l = 0; l < MAX_CONNECTIONS; l++) {
+                            if (places->place[k].connection[l] == places->place[i].id) {
+                                // Checkpoint = 3
+                                // Place[k] has Connection[l] that matches Place[i]'s ID
+                                checkpoint++;
+                                break;
                             }
-                            if (!has_connection) {
-                                return false;
-                            }
-                            break;
                         }
+                        // If checkpoint == 2
+                        // It has found the Place[k] however it didn't get a match for Connection[l] with Place[i]
+                        // If checkpoint == 3
+                        // It has found the Place[k] and matched Connection[l] with Place[i]
+                        break;
                     }
+                }
+                if (checkpoint != 3) {
+                    return false;
                 }
             }
         }
@@ -137,8 +146,8 @@ bool _evaluate_list_connection(const ListPlace *places) {
 
 bool _evaluate_list_connection_2(const ListPlace *places) {
     for (uint32_t i = 0; i < places->size; i++) {
-        for (int j = 0; j < MAX_CONNECTIONS - 1; j++) {
-            for (int k = j + 1; k < MAX_CONNECTIONS; k++) {
+        for (uint32_t j = 0; j < MAX_CONNECTIONS - 1; j++) {
+            for (uint32_t k = j + 1; k < MAX_CONNECTIONS; k++) {
                 // Repeated connections
                 if (places->place[i].connection[j] != -1 &&
                         places->place[i].connection[k] != -1 &&
@@ -157,18 +166,15 @@ void print_places(const ListPlace *places) {
 
 void print_places___(const ListPlace *places, FILE *stream, const char *title) {
     fprintf(stream, "\n----------------------\n %s:\n----------------------\n", title);
-    char buffer0[11];
-    char buffer1[11];
-    char buffer2[11];
-    for (int i = 0; i < places->size; i++) {
-        snprintf(buffer0, 11, "%d", places->place[i].connection[0]);
-        snprintf(buffer1, 11, "%d", places->place[i].connection[1]);
-        snprintf(buffer2, 11, "%d", places->place[i].connection[2]);
-        fprintf(stream, " ID: %d\n Capacity: %d\n Connections:\n  (1): %s\n  (2): %s\n  (3): %s\n\n",
-                places->place[i].id, places->place[i].capacity,
-                places->place[i].connection[0] == -1 ? "No connection." : buffer0,
-                places->place[i].connection[1] == -1 ? "No connection." : buffer1,
-                places->place[i].connection[2] == -1 ? "No connection." : buffer2);
+    char buffer[11];
+    for (uint32_t i = 0; i < places->size; i++) {
+        fprintf(stream, " ID: %d\n Capacity: %d\n Connections:\n",
+                places->place[i].id, places->place[i].capacity);
+        for (int j = 0; j < MAX_CONNECTIONS; j++) {
+            snprintf(buffer, 11, "%d", places->place[i].connection[j]);
+            fprintf(stream, "  (%d): %s\n", (j + 1), places->place[i].connection[j] == -1 ? "No connection." : buffer);
+        }
+        fprintf(stream, "\n");
     }
     fprintf(stream, "----------------------\n End.\n----------------------\n");
 }
@@ -191,12 +197,12 @@ uint32_t get_max_capacity(const ListPlace *places) {
 int32_t get_place_by_id(ListPlace *places, const int32_t id, Place **place) {
     *place = NULL;
 
-    for (int32_t i = 0; i < places->size; i++) {
+    for (uint32_t i = 0; i < places->size; i++) {
         if (places->place[i].id == id) {
             *place = &places->place[i];
             return i;
         }
     }
 
-    return (int32_t)-1;
+    return (int32_t) - 1;
 }
